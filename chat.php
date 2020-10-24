026e622f080d95762212906039d89e554086cc0a
Kenrichk <sg6157ye@go.minnstate.edu>
2020-10-24 18:38:05 -0500

    hey

--- chat.php
+++ chat.php
@@ -1,6 +1,10 @@
 <?php
+require('./dbconnect.php');
   require('./header.html');
-
+  echo'<html>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	';
   echo "<p>Under construction...</p>";
   echo "<p>This page will contain the actual chat</p> 
   <p>application and friends list</p>";
@@ -13,6 +17,28 @@
 
   //can you see me kenny
   //yeah
+  //should we use a profile pic?
+  echo '
+<div class="input-group mb-3">
+  <div class="custom-file">
+    <input type="file" class="custom-file-input btn-secondary" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+    <label class="custom-file-label" for="inputGroupFile01">++</label>
+  </div>
+  <div class="chatInput">
+    <input type="text" placeholder="message '+$recipient+'">
+  </div>
+  <div class="chatSend">
+    <button type="button" class="btn btn-danger" action="file?">
+  </div>
+</div>';//chat input
+//function deterniming if its a file, add msg after file popup like discord?
+function file?{
+  //after function (or in function?)
+  //post msg to chat
+  //im assuming youre holding the messages in the database and displaying them all in a scrolling window.
+}
+
+  echo '</html>';
 ?>
 
 
