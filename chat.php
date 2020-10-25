a3b908a2c321da23408d6fe50be6a83de4d32c92
Kenrichk <sg6157ye@go.minnstate.edu>
2020-10-25 18:25:44 -0500

    hey

--- chat.php
+++ chat.php
@@ -25,8 +25,8 @@ require('./dbconnect.php');
     <label class="custom-file-label" for="inputGroupFile01">++</label>
   </div>
   <div class="chatInput">
-    <input type="text" placeholder="message '+$recipient+'">
-  </div>
+   '+// <input type="text" placeholder="message '+$recipient+'">
+  '</div>
   <div class="chatSend">
     <button type="button" class="btn btn-danger" action="file?">
   </div>
