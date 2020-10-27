a2c3b68a7f9660b530ae566a0a78274cf584ee34
Kenrichk <sg6157ye@go.minnstate.edu>
2020-10-27 12:43:11 -0500

    hey

--- chat.php
+++ chat.php
@@ -18,7 +18,7 @@ require('./dbconnect.php');
   echo '
 <div class="input-group mb-3">
   <div class="custom-file">
-  <button type="button" class="btn-secondary"action="getFile">+</button>
+  <button type="button" class="btn btn-secondary"action="getFile">+</button>
   <!--//instead of using a file input diectly on the message thing, lets use a butoon
   //that onPress, it opens up a little window like in discord, and the file input is in
   //that window, which upon opening, it automatically clicks-->
